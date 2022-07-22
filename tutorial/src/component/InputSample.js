8d1e9ab7138735c31b97bbd129552705bde2e0a3
김창휘 <kch717923@gmail.com>
2022-07-22 17:03:40 +0900

    일단 남겨놔

--- tutorial/src/component/InputSample.js
+++ tutorial/src/component/InputSample.js
@@ -1,19 +1,23 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const InputSample = () => {
 
-    const [text,setText] = useState('');
+    const [text,setText] = useState('뭐');
 
     const onChange = (e) => {
-        setText(e.target.value)
+        setText(e.target.innerText)
     }
 
     const onReset = () => {
         setText('');
     }
+    useEffect(() => {
+        console.log(text)
+    })
     return (
         <div>
             <input onChange={onChange} value={text}/>
+            <div onInput={onChange} contentEditable={true} value={text}></div>
             <button onClick={onReset}>초기화</button>
             <div>
                 <b>값 : {text}</b>
