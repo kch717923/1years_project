c2cc59e209f7c6d7b3100dde4b69e50a82c71052
김창휘 <kch717923@gmail.com>
2022-07-25 17:53:00 +0900

    다중 input 상태관리

--- tutorial/src/component/InputSample.js
+++ tutorial/src/component/InputSample.js
@@ -1,26 +1,38 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 
 const InputSample = () => {
 
-    const [text,setText] = useState('뭐');
+    const [inputs,setInputs] = useState({
+        name:'',
+        nickname:''
+    })
 
+    const {name,nickname} = inputs;
     const onChange = (e) => {
-        setText(e.target.innerText)
+        console.log(e)
+        console.log(e.target.attributes["data-test"].value)
+        const {value,name} = e.target;
+        setInputs({
+            ...inputs,
+            [name]:value
+        })
     }
 
     const onReset = () => {
-        setText('');
+        setInputs({
+            name:'',
+            nickname:''
+        })
     }
-    useEffect(() => {
-        console.log(text)
-    })
+
     return (
         <div>
-            <input onChange={onChange} value={text}/>
-            <div onInput={onChange} contentEditable={true} value={text}></div>
+            <input name="name" placeholder="이름" onChange={onChange} value={name} data-test="불러바바"/>
+            <input name="nickname" placeholder="닉네임" onChange={onChange} value={nickname}/>
             <button onClick={onReset}>초기화</button>
             <div>
-                <b>값 : {text}</b>
+                <b>값 : </b>
+                {name} ({nickname})
             </div>
         </div>
     );
